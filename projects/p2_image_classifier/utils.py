54154665598900325986e66d1fec9593e6322279
rahmafares98 <rahmafaris981@gmail.com>
2025-05-31 17:01:42 +0000

    finalone

--- projects/p2_image_classifier/utils.py
+++ projects/p2_image_classifier/utils.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import numpy as np
 from PIL import Image
+import tensorflow_hub as hub  
 
 def process_image(image_path):
     image = Image.open(image_path).resize((224, 224))
@@ -8,7 +9,4 @@ def process_image(image_path):
     return image
 
 def load_model(model_path):
-    if model_path.endswith('.h5'):
-        return tf.keras.models.load_model(model_path, custom_objects={'KerasLayer': tf.keras.layers.Layer})
-    else:
-        return tf.keras.models.load_model(model_path)
+    return tf.keras.models.load_model(model_path, custom_objects={'KerasLayer': hub.KerasLayer})
